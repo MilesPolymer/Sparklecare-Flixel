08f3453cf385ea3cfdceeed1bd25eb19792206c6
FizzyBot <mranimationmaker27flip@gmail.com>
2023-05-29 11:46:07 -0400

    uni

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -8,18 +8,27 @@ import flixel.tweens.FlxTween;
 
 class PlayState extends FlxState
 {
-	var sprite:FlxSprite;
+	var barrySprite:FlxSprite;
+
+	var uniSprite:FlxSprite;
 
 	override public function create()
 	{
 		super.create();
 
-		sprite = new FlxSprite();
-		sprite.loadGraphic(AssetPaths.barryIll__png);
-		sprite.x = 90;
-		sprite.y = 0;
-		add(sprite);
-		FlxTween.tween(sprite, {x: FlxG.width - sprite.width, y: FlxG.height - sprite.height, angle: 360.0}, 5, {type: FlxTween.PINGPONG});
+		barrySprite = new FlxSprite();
+		barrySprite.loadGraphic(AssetPaths.barryIll__png);
+		barrySprite.x = 90;
+		barrySprite.y = 0;
+		add(barrySprite);
+		FlxTween.tween(barrySprite, {x: FlxG.width - barrySprite.width, y: FlxG.height - barrySprite.height, angle: 360.0}, 5, {type: FlxTween.PINGPONG});
+
+		uniSprite = new FlxSprite();
+		uniSprite.loadGraphic(AssetPaths.uniModel__png);
+		uniSprite.x = 90;
+		uniSprite.y = 0;
+		add(uniSprite);
+		FlxTween.tween(uniSprite, {x: FlxG.width - uniSprite.width, y: FlxG.height - uniSprite.height, angle: 360.0}, 5, {type: FlxTween.PINGPONG});
 	}
 
 	override public function update(elapsed:Float)
